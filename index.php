c14b345ebf0da51e238d62915ba3617ec55388e2
san0031 <san0031@gmail.com>
2018-11-07 19:27:58 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World. Thiis is first openshift php project .</p>'; ?> 
+ <?php echo '<p>Hello World. Thiis is first openshift php project again online .</p>'; ?> 
  </body>
 </html>
